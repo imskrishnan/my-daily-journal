5f465e93d90b5958e8180f7304068cb2e61d8ff6
imskrishnan <imskrishnan@gmail.com>
2020-12-25 23:35:21 +0530

    Add DateFormat functionality to display for each post

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ const posts = [];
 const homeStartingContent = "Welcome to my Daily Journal! This is website is simply everything me!"
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const today = new Date();
+
+//This is the dateFunctionality
+function getDate() {
+  return (today.toLocaleDateString("en-US", {
+  day: 'numeric'
+}) + " / " + today.toLocaleDateString("en-US", {
+  month: 'short'
+}) + " / " + today.toLocaleDateString("en-US", {
+  year: 'numeric'
+}))}
 
 const app = express();
 
@@ -18,6 +29,7 @@ app.use(express.static("public"));
 
 app.get('/', (req, res) => {
   res.render("home", {
+    date: getDate(),
     data: posts,
     title: "Home",
     homePara: homeStartingContent});
@@ -31,6 +43,7 @@ app.get('/post/:section', (req, res) => {
       console.log("postItem : " + _.toLower(_.lowerCase(postItem)));
       console.log("postTitle : " + _.lowerCase(e.title));
       res.render('post', {
+        currentDateAndTime: getDate(),
         composeTitle: e.title,
         composeBody: e.content
       });
